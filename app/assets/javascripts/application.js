91e65c1cc6b9e0d0bd48d264a46313a4db4e814c
Eben English <eenglish@bpl.org>
2012-12-20 15:28:05 -0500

    still working on modal image viewer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,29 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
 //
 // Required by Blacklight
-//= require blacklight/blacklight
-//= require_tree .
+// require blacklight/blacklight
+
+//= require blacklight/core
+//= require blacklight/bookmark_toggle
+//= require blacklight/facet_expand_contract
+// require blacklight/lightbox_dialog
+//= require blacklight/search_context
+//= require blacklight/select_submit
+//= require blacklight/zebra_stripe
+//= require blacklight/css_dropdowns
+
+//Bootstrap JS for providing collapsable tablet/mobile menu/alert boxes
+//= require bootstrap-transition
+//= require bootstrap-collapse
+//= require bootstrap-alert
+//= require improved-modal/bootstrap-modalmanager.js
+//= require improved-modal/bootstrap-modal.js
+
+
+// WARNING: require tree has been disabled to avoid djatoka JS files being automatically included
+//          since the viewer uses Prototype, which causes namespace conflicts with jQuery
+// WARNING: to require any further app-specific JS, you must use the 'require_directory' directive
+// require_tree .
