f64e90f26b5f12cc52545b6c93f4aa55b6aee550
Eben English <eenglish@bpl.org>
2013-04-05 12:03:01 -0400

    small fixes for production djatoka viewer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
-//= require 'blacklight_advanced_search'
+//= require blacklight_advanced_search/blacklight_advanced_search_javascript
 
 
 //= require jquery_ujs
