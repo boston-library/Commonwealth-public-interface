350bee5f241dcda2dfa76cb227ed7e01f81b13d9
Eben English <eenglish@bpl.org>
2013-05-17 13:45:36 -0400

    use Rails.cache for DC RSS feed on home page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -7,17 +7,24 @@ module PagesHelper
     feed = Rails.cache.fetch('dc_rss_feed', :expires_in => 60.minutes) do
       RSS::Parser.parse(open(source).read, false).items[0..3]
     end
-    #
-    content = []
-    feed.each do |item|
-      content << content_tag(:li,
-                             link_to(item.title,
-                                     item.link,
-                                     :class => 'feed_item_link',
-                                     :target => '_blank'),
-                             :class => 'feed_item')
+    if feed.length > 0
+      content = []
+      feed.each do |item|
+        content << content_tag(:li,
+                               link_to(item.title,
+                                       item.link,
+                                       :class => 'feed_item_link',
+                                       :target => '_blank'),
+                               :class => 'feed_item')
+      end
+      content_tag(:ul, content.join().html_safe, :class => 'feed_items')
+    else
+      content_tag(:p,
+                  'Visit the ' + link_to('Digital Commonwealth News Blog',
+                                         'http://digitalcommonwealth.org/blog/',
+                                         :target => '_blank') + ' for the latest updates.')
     end
-    content_tag('ul', content.join().html_safe, :class => 'feed_items')
+
   end
 
 
