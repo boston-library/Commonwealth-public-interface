55cf92125c742109d3cf3a9247b4792b2f9488e7
Eben English <eenglish@bpl.org>
2014-03-14 14:07:14 -0400

    decent (for now) book viewing via multi_img_viewer

--- config/initializers/djatoka.rb
+++ config/initializers/djatoka.rb
@@ -1,1 +1,1 @@
-Rails.configuration.djatoka_resolver = Djatoka::Resolver.new('https://fedoratest.bpl.org/adore-djatoka/resolver')
+Rails.configuration.djatoka_resolver = Djatoka::Resolver.new('https://fedoradev.bpl.org/adore-djatoka/resolver')
