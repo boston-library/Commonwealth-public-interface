cd99cd866127260139a86e6874691dbde69bd16d
Eben English <eenglish@bpl.org>
2014-04-09 15:56:04 -0400

    more tests for pages_controller

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -16,7 +16,39 @@ describe PagesController do
     it 'should show the about page' do
       get 'about'
       response.should be_success
-      response.body.should have_selector('#about_content')
+      response.body.should have_selector('ul.page_list')
+    end
+  end
+
+  describe "GET 'about_dc'" do
+    it 'should show the about_dc page' do
+      get 'about_dc'
+      response.should be_success
+      response.body.should have_selector('div.about_content')
+    end
+  end
+
+  describe "GET 'lesson_plans'" do
+    it 'should show the lesson_plans page' do
+      get 'lesson_plans'
+      response.should be_success
+      response.body.should have_selector('#lesson_plan_list')
+    end
+  end
+
+  describe "GET 'copyright'" do
+    it 'should show the copyright page' do
+      get 'copyright'
+      response.should be_success
+      response.body.should have_selector('div.about_content')
+    end
+  end
+
+  describe "GET 'partners'" do
+    it 'should show the partners page' do
+      get 'partners'
+      response.should be_success
+      response.body.should have_selector('#partner_list')
     end
   end
 
