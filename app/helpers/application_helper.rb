3cb821f86a6796bbafef5a39210d70567a6ca918
Eben English <eenglish@bpl.org>
2014-04-17 12:46:26 -0400

    move helper methods to appropriate helper file based on scope

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -105,24 +105,6 @@ module ApplicationHelper
     link_to(field + ' County', new_params)
   end
 
-  def simpleimage_file_pid (document)
-    return Bplmodels::Image.find(document[:id]).image_files.first.pid
-  end
-
-  def render_item_breadcrumb(document)
-    if document[:institution_pid_ssi] && document[:collection_pid_ssm]
-      inst_link = link_to(document[:institution_name_ssim].first,
-                          institution_path(:id => document[:institution_pid_ssi]),
-                          :class => 'institution_breadcrumb')
-      connector = content_tag(:i, '',
-                              :class => 'icon-arrow-right item-breadcrumb-separator')
-      coll_link = link_to(document[:collection_name_ssim].first,
-                          collection_path(:id => document[:collection_pid_ssm].first),
-                          :class => 'collection_breadcrumb')
-      inst_link + connector + coll_link
-    end
-  end
-
   def datastream_disseminator_url pid, datastream_id
     ActiveFedora::Base.connection_for_pid(pid).client.url + "/objects/#{pid}/datastreams/#{datastream_id}/content"
   end
@@ -130,17 +112,6 @@ module ApplicationHelper
   # create djatoka-friendly non-ssl image path
   def nonssl_image_uri(pid,datastream_id)
     datastream_disseminator_url(pid,datastream_id).gsub(/\Ahttps/,'http')
-  end
-
-  def has_image_files? files_hash
-    image_file_pids = nil
-    unless files_hash[:images].empty?
-      image_file_pids = []
-      files_hash[:images].each do |image_file|
-        image_file_pids << image_file['id']
-      end
-    end
-    image_file_pids
   end
 
   def create_thumb_img_element(thumb_pid, resource_type, doc_title, img_class)
@@ -155,73 +126,6 @@ module ApplicationHelper
         render_object_icon(nil, img_class)
       end
     end
-  end
-
-  def render_mods_dates (date_start, date_end = nil, date_qualifier = nil, date_type = nil)
-    prefix = ''
-    suffix = ''
-    date_start_suffix = ''
-    if date_qualifier
-      prefix = date_qualifier == 'approximate' ? '[ca. ' : '['
-      suffix = date_qualifier == 'questionable' ? '?]' : ']'
-    end
-    prefix << 'c' if date_type == 'copyrightDate'
-    if date_end && date_end != 'nil'
-      date_start_suffix = '?' if date_qualifier == 'questionable'
-      prefix + normalize_date(date_start) + date_start_suffix + t('blacklight.metadata_display.fields.date.date_range_connector') + normalize_date(date_end) + suffix
-    else
-      prefix + normalize_date(date_start) + suffix
-    end
-  end
-
-  def render_mods_xml_record(document_id)
-    mods_xml_file_path = datastream_disseminator_url(document_id, 'descMetadata')
-    mods_response = Typhoeus::Request.get(mods_xml_file_path)
-    mods_xml_text = REXML::Document.new(mods_response.body)
-  end
-
-  def normalize_date(date)
-    if date.length == 10
-      Date.parse(date).strftime('%B %-d, %Y')
-    elsif date.length == 7
-      Date.parse(date + '-01').strftime('%B %Y')
-    else
-      date
-    end
-  end
-
-  # insert an icon and link to CC licenses
-  def render_cc_license(terms)
-    terms_code = terms.match(/\s[BYNCDSA-]{2,}/).to_s.strip.downcase
-    link_to(image_tag('//i.creativecommons.org/l/' + terms_code + '/3.0/80x15.png',
-                      :alt => 'CC ' + terms_code.upcase + ' icon',
-                      :class => 'cc_license_icon'),
-            'http://creativecommons.org/licenses/' + terms_code + '/3.0',
-            :rel => 'license',
-            :id => 'cc_license_link',
-            :target => '_blank')
-  end
-
-  def has_downloadable_files? files_hash
-    files_hash[:documents].present? ||
-    files_hash[:audio].present? ||
-    files_hash[:generic].present?
-  end
-
-  def create_download_links(files_hash, link_class)
-    file_types = [files_hash[:documents], files_hash[:audio], files_hash[:generic]]
-    download_links = []
-    file_types.each do |file_type|
-      file_type.each do |file|
-        object_profile_json = JSON.parse(file['object_profile_ssm'].first)
-        file_name_ext = object_profile_json["objLabel"].split('.')
-        download_links << link_to(file_name_ext[0],
-                                  download_path(file['id'],:datastream_id => 'productionMaster')                                  ,
-                                  :target => '_blank',
-                                  :class => link_class) + ' (' + file_name_ext[1].upcase + ', ' + number_to_human_size(object_profile_json["datastreams"]["productionMaster"]["dsSize"]) + ')'
-      end
-    end
-    download_links
   end
 
   def insert_google_analytics
