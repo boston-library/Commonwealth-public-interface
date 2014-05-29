3a8dfd63b736d4f92e326e053194d401b4ead463
Eben English <eenglish@bpl.org>
2014-05-29 17:23:12 -0400

    more updates for responsive styles for BL/Bootstrap upgrade

--- app/helpers/facets_helper.rb
+++ app/helpers/facets_helper.rb
@@ -1,6 +1,6 @@
 module FacetsHelper
   include Blacklight::FacetsHelperBehavior
-
+=begin
   # for display of series facets in collections#show
   def render_series_facet_value(facet_solr_field, item, options ={})
     series_title = facet_display_value(facet_solr_field, item)
@@ -10,5 +10,5 @@ module FacetsHelper
     link_to(series_img, add_facet_params_and_redirect(facet_solr_field, item))
     (link_to(series_title, add_facet_params_and_redirect(facet_solr_field, item), :class=>"facet_select") + " (" + render_facet_count(item.hits) + " items)").html_safe
   end
-
+=end
 end
