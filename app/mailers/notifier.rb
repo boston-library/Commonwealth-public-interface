6ad1580944934c0144f47432e661f244a25b4e40
Eben English <eenglish@bpl.org>
2014-06-10 17:28:07 -0400

    initial work on routing feedback form emails by topic

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -6,11 +6,25 @@ class Notifier < ActionMailer::Base
     @message = details[:message]
     @email = details[:email]
     @name = details[:name]
+    @recipient = route_email(details[:topic])
 
-    mail(:to => t('blacklight.repo-admin.email'),
+    mail(:to => @recipient,
          :from => t('blacklight.email.record_mailer.name') + ' <' + t('blacklight.email.record_mailer.email') + '>',
          :subject => t('blacklight.feedback.text.subject'))
 
+  end
+
+  private
+
+  def route_email(topic)
+    if topic == t('blacklight.feedback.form.topic.options.membership')
+      recipient_email = CONTACT_EMAILS['dc_admin']
+    elsif topic == t('blacklight.feedback.form.topic.options.repro')
+      recipient_email = CONTACT_EMAILS['image_requests']
+    else
+      recipient_email = CONTACT_EMAILS['bpl_admin']
+    end
+    recipient_email
   end
 
 end
--- config/initializers/load_project_configs.rb
+++ config/initializers/load_project_configs.rb
@@ -1,3 +1,5 @@
 # various app-specific config settings
 
 GOOGLE_ANALYTICS = YAML.load_file(Rails.root.join('config', 'google_analytics.yml'))[Rails.env]
+
+CONTACT_EMAILS = YAML.load_file(Rails.root.join('config', 'contact_emails.yml'))[Rails.env]
