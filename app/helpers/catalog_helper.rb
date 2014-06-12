21b8755f082d41c4e294aee1a015a37c6e74db69
Eben English <eenglish@bpl.org>
2014-06-12 15:59:44 -0400

    display modal warning for flagged content on catalog#show

--- app/helpers/catalog_helper.rb
+++ app/helpers/catalog_helper.rb
@@ -109,9 +109,9 @@ module CatalogHelper
     mods_xml_text = REXML::Document.new(mods_response.body)
   end
 
-  def create_thumb_img_element(document, img_class=[])
+  def create_thumb_img_element(document, img_class=[], action=nil)
     image_classes = img_class.join(' ')
-    if document[:exemplary_image_ssi] and !document[blacklight_config.flagged_field.to_sym]
+    if document[:exemplary_image_ssi] && (!document[blacklight_config.flagged_field.to_sym] || (action && action == 'show'))
       image_tag(datastream_disseminator_url(document[:exemplary_image_ssi], 'thumbnail300'),
                 :alt => document[blacklight_config.index.title_field.to_sym],
                 :class => image_classes)
