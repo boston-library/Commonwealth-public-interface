a2896e7ae87ce7e932057ff9af2440a4ca003347
Eben English <eenglish@bpl.org>
2014-06-13 09:39:23 -0400

    set flagged field name to flagged_content_ssi

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -36,7 +36,7 @@ class CatalogController < ApplicationController
     config.show.display_type_field = 'active_fedora_model_suffix_ssi'
 
     # solr field for flagged/inappropriate content
-    config.flagged_field = 'institution_name_ssim'
+    config.flagged_field = 'flagged_content_ssi'
 
     # solr fields that will be treated as facets by the blacklight application
     #   The ordering of the field names is the order of the display
