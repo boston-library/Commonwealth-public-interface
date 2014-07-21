8cfb56b9f545f990b9530082593b972c29982196
Eben English <eenglish@bpl.org>
2014-07-21 10:36:31 -0400

    have to wait until all object have workflow_state_ssi field before using to exclude unwanted objects in Solr response

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -231,8 +231,10 @@ class CatalogController < ApplicationController
   def exclude_unwanted_models(solr_parameters, user_parameters)
     solr_parameters[:fq] ||= []
     solr_parameters[:fq] << '-has_model_ssim:"info:fedora/afmodel:Bplmodels_File"'
-    solr_parameters[:fq] << '+workflow_state_ssi:"published"'
+    solr_parameters[:fq] << '-workflow_state_ssi:"draft"'
+    solr_parameters[:fq] << '-workflow_state_ssi:"needs_review"'
     # can't implement below until all records have this field
+    # solr_parameters[:fq] << '+workflow_state_ssi:"published"'
     # solr_parameters[:fq] << '+processing_state_ssi:"complete"'
   end
 
