4ea1c0f54aa8ea4612941ab3b9a7198ce2ff3610
Eben English <eenglish@bpl.org>
2014-09-04 10:53:51 -0400

    return image icon for flagged items in PreviewController#show

--- app/controllers/preview_controller.rb
+++ app/controllers/preview_controller.rb
@@ -6,29 +6,32 @@ class PreviewController < CatalogController
 
   copy_blacklight_config_from(CatalogController)
 
+  # return an image file for <mods:url access='preview'> requests
+  # for flagged items, return the image icon
   def show
     @response, @document = get_solr_response_for_doc_id
-    # the conditionals below don't really work, because a bad id returned by
-    # get_solr_response_for_doc_id triggers a rescue action in BL/lib/catalog.rb,
-    # which causes an immediate escape, and any code below that isn't evaluated
-    # need to figure out how to override this
     if @document[:exemplary_image_ssi]
-      thumb_prefix = @document[:id].to_s
-      @image_pid = @document[:exemplary_image_ssi]
-      @thumb_datastream_url = view_context.datastream_disseminator_url(@image_pid, 'thumbnail300')
-      response = Typhoeus::Request.get(@thumb_datastream_url)
-      if response.headers[/404 Not Found/]
-        not_found
-      else
-        send_data response.body,
-                  :filename => thumb_prefix + '_thumbnail.jpg',
-                  :type => :jpg,
+      filename = @document[:id].to_s + '_thumbnail'
+      if @document[blacklight_config.flagged_field.to_sym]
+        send_file File.join(Rails.root, 'app', 'assets', 'images', 'dc_image-icon.png'),
+                  :filename => filename + '.png',
+                  :type => :png,
                   :disposition => 'inline'
+      else
+        thumb_datastream_url = view_context.datastream_disseminator_url(@document[:exemplary_image_ssi], 'thumbnail300')
+        response = Typhoeus::Request.get(thumb_datastream_url)
+        if response.headers[/404 Not Found/]
+          not_found
+        else
+          send_data response.body,
+                    :filename => filename + '.jpg',
+                    :type => :jpg,
+                    :disposition => 'inline'
+        end
       end
     else
       not_found
     end
-
   end
 
   private
@@ -36,15 +39,5 @@ class PreviewController < CatalogController
   def not_found
     raise ActionController::RoutingError.new('Not Found')
   end
-
-  #def get_thumb_url(pid)
-    # item = Bplmodels::ObjectBase.find("bpl-test:mc87pq30p")
-    # item.datastreams
-    # item.rels_ext
-    # relsext-rdf = item.rels_ext.relationships # returns an RDF::Graph object
-    # relsext-rdf.triples    # returns an enumerator of triples
-    # relsext-rdf.triples.first  # returns an triple as an array
-    # relsext-rdf.triples.first.[0]  # access one part of a triple, call to_s, etc.
-  #end
 
 end
--- spec/controllers/preview_controller_spec.rb
+++ spec/controllers/preview_controller_spec.rb
@@ -2,6 +2,26 @@ require 'spec_helper'
 
 describe PreviewController do
 
-  #TODO: um, add some tests
+  describe 'GET #show' do
+
+    it 'should return the thumbnail if one exists' do
+      get :show, :id => 'bpl-dev:k930bx42b'
+      expect(response).to be_success
+      response.header['Content-Type'].should == 'image/jpeg'
+    end
+
+    it 'should return the image icon if this is a flagged item' do
+      get :show, :id => 'bpl-dev:9880vr198'
+      expect(response).to be_success
+      response.header['Content-Type'].should == 'image/png'
+    end
+
+    it 'should return a 404 error if no image exists' do
+      get :show, :id => 'xyz123'
+      expect(response).to_not be_success
+      response.response_code.should == 404
+    end
+
+  end
 
 end
