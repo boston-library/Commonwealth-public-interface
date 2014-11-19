68c54067603e0c61e2ebc6ad9949de89c631b227
Eben English <eenglish@bpl.org>
2014-11-19 10:23:30 -0500

    more fixes for including institution name value in collection 'view all' links

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -1,9 +1,10 @@
 module CollectionsHelper
 
   # link to view all items in a collection
-  def link_to_all_col_items(col_title,link_class)
+  def link_to_all_col_items(col_title, institution_name, link_class)
     link_to(t('blacklight.collections.browse.all'),
-            catalog_index_path(:f => {blacklight_config.collection_field => [col_title]}),
+            catalog_index_path(:f => {blacklight_config.collection_field => [col_title],
+                                      blacklight_config.institution_field => institution_name}),
             :class => link_class)
   end
 
