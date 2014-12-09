810de5bf2a063f447f24fdd351fc316c761f0ab5
Eben English <eenglish@bpl.org>
2014-12-09 15:37:17 -0500

    better parsing of subject_geojson_facet_ssim placenames

--- app/helpers/blacklight_maps_helper.rb
+++ app/helpers/blacklight_maps_helper.rb
@@ -5,11 +5,12 @@ module BlacklightMapsHelper
   def link_to_placename_facet field_value, field, displayvalue = nil
     new_params = params
     field_values = field_value.split(', ')
-    if field_values.length > 1
-      if field_values.last.length == 2
-        state_name = Bplmodels::Constants::STATE_ABBR[field_values.last]
-        field_values[field_values.length-1] = state_name if state_name
-      end
+    if field_values.length > 2 || field_values.last.match(/[\.\)]/) # Mass.)
+      field_values = [field_values.join(', ')]
+    end
+    if field_values.length == 2 && field_values.last.length == 2
+      state_name = Bplmodels::Constants::STATE_ABBR[field_values.last]
+      field_values[field_values.length-1] = state_name if state_name
     end
     field_values.each do |val|
       new_params = add_facet_params(field, val, new_params) unless params[:f] && params[:f][field] && params[:f][field].include?(val)
