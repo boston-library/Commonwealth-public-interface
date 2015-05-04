7ecfe425b7b08e2a1369adec1ee140a194fef25c
Eben English <eenglish@bpl.org>
2015-05-04 10:58:15 -0400

    updates to folders/bookmarks for updated BL

--- app/helpers/folders_helper.rb
+++ app/helpers/folders_helper.rb
@@ -1,8 +1,7 @@
 module FoldersHelper
 
   def folder_belongs_to_user
-    this_user = current_or_guest_user
-    if this_user.folders.include? @folder
+    if current_or_guest_user.folders.include? @folder
       true
     else
       false
