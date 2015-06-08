d75dff2ac15757e7fc851b31d9321b59f7142962
Eben English <eenglish@bpl.org>
2015-06-08 10:05:53 -0400

    moved institutions behavior into vlr-engine

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -1,10 +1,4 @@
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
 
-  # used by InstitutionsController#index
-  def institutions_filter(solr_parameters = {})
-    solr_parameters[:fq] ||= []
-    solr_parameters[:fq] << "+active_fedora_model_suffix_ssi:\"Institution\""
-  end
-
 end
