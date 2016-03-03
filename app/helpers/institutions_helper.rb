28c76957b7e773448e921884aa6c4dcddbf93a58
Eben English <eenglish@bpl.org>
2016-03-03 11:38:54 -0500

    override #should_render_inst_az? from commonwealth-vlr-engine

---
+++ app/helpers/institutions_helper.rb
@@ -0,0 +1,9 @@
+module InstitutionsHelper
+  include CommonwealthVlrEngine::InstitutionsHelperBehavior
+
+  # whether the A-Z link menu should be displayed in institutions#index
+  def should_render_inst_az?
+    true
+  end
+
+end
