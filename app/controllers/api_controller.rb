7c032e0f6756dae481bbdaee759246006890c276
Steven Anderson <sanderson@bpl.org>
2016-04-15 10:52:09 -0400

    Get some better information on why user is invalid

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,32 +3,37 @@ class ApiController < ActionController::Base
   before_action :validate_pw, :except=>[:digital_stacks_login]
 
   def digital_stacks_create
+    debug_user_logger = Logger.new('log/digital_stacks_create.log')
+    debug_user_logger.level = Logger::DEBUG
+
     request_json = JSON.parse(request.body.read)
     begin
       if request_json["barcode"]["type"] == 'mbln'
         barcode = request_json["barcode"]["value"]
+        debug_user_logger.debug "Barcode is: #{barcode}"
 
         lookup = BplApi::Polaris.new
         lookup_response = lookup.get_user_data(barcode)
+        debug_user_logger.debug "Lookup response is: #{lookup_response.to_yaml}"
         if lookup_response.blank? || lookup_response.info[:valid_patron] == 'false'
           respond_to do |format|
             format.json { render json: {"result" => 'Invalid barcode', 'message' => "The library card barcode appears to be invalid", 'contact'=> 'sanderson@bpl.org'}.as_json, status: 500 }
           end
         else
           @user = User.find_for_polaris_oauth(lookup_response)
+          debug_user_logger.debug "User response is: #{@user.to_yaml}"
 
 
           target_folder = @user.folders.where(:title=>request_json["name"])
           if target_folder.blank?
             target_folder = @user.folders.new
-            target_folder.user = @user
-            target_folder.title = request_json["name"][0..39]
+            target_folder.title = request_json["name"]
             target_folder.description = request_json["name"]
             target_folder.visibility = 'private'
-            target_folder.save!
+            debug_user_logger.debug "Target Folder User Is: #{target_folder.user.to_yaml}"
+            target_folder.save
           else
             target_folder = target_folder.first
-            target_folder.user = @user
           end
           request_json["items"].each do |item_to_add|
             target_folder.folder_items.create!(:document_id => item_to_add) and target_folder.touch unless target_folder.has_folder_item(item_to_add)
@@ -48,7 +53,7 @@ class ApiController < ActionController::Base
 
     rescue => error
       respond_to do |format|
-        format.json { render json: {"result" => 'Caught an error', 'message' => "#{error.message}. #{error.backtrace}", 'contact'=> 'sanderson@bpl.org'}.as_json, status: 500 }
+        format.json { render json: {"result" => 'Caught an error for ', 'message' => "#{error.message}.", 'contact'=> 'sanderson@bpl.org'}.as_json, status: 500 }
       end
     end
   end
