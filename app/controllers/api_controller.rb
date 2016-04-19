8754d08009d5d95dab4a6a0aff47fa2ab7662747
Steven Anderson <sanderson@bpl.org>
2016-04-19 10:25:36 -0400

    Add better password check and logging

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -31,7 +31,7 @@ class ApiController < ActionController::Base
             target_folder.description = request_json["name"]
             target_folder.visibility = 'private'
             debug_user_logger.debug "Target Folder User Is: #{target_folder.user.to_yaml}"
-            target_folder.save
+            target_folder.save!
           else
             target_folder = target_folder.first
           end
@@ -44,8 +44,15 @@ class ApiController < ActionController::Base
           end
         end
 
-      else
+      elsif  request_json["barcode"]["type"] == 'temporary'
+        user_details = {}
+        user_details[:uid] = request_json["barcode"]["value"]
 
+
+        respond_to do |format|
+          format.json { render json: {"result" => 'added to account', 'message' => "Added items to folder #{request_json["name"]}", 'contact'=> 'sanderson@bpl.org'}.as_json, status: 200 }
+        end
+      else
         respond_to do |format|
           format.json { render json: {"result" => 'Not implemented yet', 'message' => "Only mbln implemented thus far", 'contact'=> 'sanderson@bpl.org'}.as_json, status: 404 }
         end
@@ -59,7 +66,21 @@ class ApiController < ActionController::Base
   end
 
   def validate_pw
-    if pw != request["password"]
+    debug_user_logger = Logger.new('log/digital_stacks_create.log')
+    debug_user_logger.level = Logger::DEBUG
+    debug_user_logger.debug "------------------------------------"
+
+    request_json = nil
+    begin
+      request_json = JSON.parse(request.body.read)
+    rescue
+      respond_to do |format|
+        format.json { render json: {"result" => 'Invalid JSON', 'message' => 'Could not parse the JSON hash', 'contact'=> 'sanderson@bpl.org'}.as_json, status: 500 }
+      end
+    end
+
+    if pw != request_json["password"]
+      debug_user_logger.debug "Full Request Content is: #{request_json.to_yaml}"
       respond_to do |format|
         format.json { render json: {"result" => 'Unauthorized', 'message' => 'Your password was incorrect', 'contact'=> 'sanderson@bpl.org'}.as_json, status: 401 }
       end
