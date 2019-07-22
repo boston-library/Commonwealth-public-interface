438cccb9d938ce8935d7d3d7e4853c5ccfa19502
Eben English <eenglish@bpl.org>
2019-07-22 11:00:36 -0400

    route 'research question' contact emails to separate email address

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -9,6 +9,8 @@ class Notifier < ActionMailer::Base
       recipient_email = CONTACT_EMAILS['dc_admin']
     elsif topic == t('blacklight.feedback.form.topic.options.repro.option')
       recipient_email = CONTACT_EMAILS['image_requests']
+    elsif topic == t('blacklight.feedback.form.topic.options.research.option')
+      recipient_email = CONTACT_EMAILS['research_question']
     else
       recipient_email = CONTACT_EMAILS['site_admin']
     end
