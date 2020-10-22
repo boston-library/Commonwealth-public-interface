66f481fcbb6b1d939c580f9f215d8a02150409d6
Eben English <eben.english@gmail.com>
2020-10-22 18:05:07 -0400

    ran rails app:update

--- config/application.rb
+++ config/application.rb
@@ -11,13 +11,10 @@ module CommonwealthPublicInterface
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.autoload_paths += %W(#{config.root}/lib)
-
-    config.i18n.enforce_available_locales = true
-
     unless Rails.env.test?
       config.middleware.use Rack::Attack
     end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
