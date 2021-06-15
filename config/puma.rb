7b4184dde33ed82ada47713464775ef09f4f6356
Eben English <eben.english@gmail.com>
2021-06-15 12:50:56 -0400

    additional config for puma/staging [skip ci]

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -17,7 +19,7 @@ environment rails_env
 worker_timeout 3600 if rails_env == 'development'
 
 if %w(staging production).member?(rails_env)
-  bind "unix://#{app_dir}/tmp/pids/commonwealth_public_puma.sock"
+  bind "unix://#{app_dir}/tmp/sockets/commonwealth_public_puma.sock"
   stdout_redirect("#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true)
   pidfile "#{app_dir}/tmp/pids/commonwealth_public_puma_server.pid"
   state_path "#{app_dir}/tmp/pids/commonwealth_public_puma_server.state"
