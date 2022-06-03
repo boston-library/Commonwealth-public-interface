fdf3fb619b4dd1c97f4a27442cb8a127c7708104
Eben English <eben.english@gmail.com>
2022-06-03 09:10:57 -0400

    rubocop cleanup for 82f486e

--- app/helpers/primary_source_sets_helper.rb
+++ app/helpers/primary_source_sets_helper.rb
@@ -1,9 +1,7 @@
-# filestream_disseminator_url(document[:exemplary_image_key_base_ss], 'image_thumbnail_300')
 # frozen_string_literal: true
 
 module PrimarySourceSetsHelper
-
-  # return the URL for the primary source set menu item
+  # return the URL for the primary source set menu item thumbnail
   # @param menu_item_data [Hash]
   # @return [String]
   def pss_thumbnail_url(menu_item_data)
