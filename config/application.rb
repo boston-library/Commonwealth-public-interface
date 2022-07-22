f174a921b7394ceefad29c87ded4793cb4fcae19
bbarberBPL <bbarber@bpl.org>
2022-07-22 11:54:45 -0400

    Use classic autoloader

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,8 @@ module CommonwealthPublicInterface
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.autoloader = :classic
+    
     config.middleware.use Rack::Attack unless Rails.env.test?
 
     # Settings in config/environments/* take precedence over those specified here.
