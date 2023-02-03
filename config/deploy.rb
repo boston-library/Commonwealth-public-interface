5f5a667faaa3cebbbb8f9ad0a162deea68db5d4e
miao-bpl <rmiao@bpl.org>
2023-02-03 14:09:44 -0500

    separating different environments

--- config/deploy.rb
+++ config/deploy.rb
@@ -106,11 +106,11 @@ namespace :boston_library do
   end
 end
 
-after :'bundler:config', :'boston_library:gem_update'
+after :'deploy:updating', :'boston_library:gem_update'
 after :'boston_library:gem_update', :'boston_library:rvm_install_ruby'
 after :'boston_library:rvm_install_ruby', :'boston_library:install_bundler'
 after :'boston_library:install_bundler', :'bundler:config'
-before :'bundler:config', :'bundle:install'
+after :'bundler:config', :'bundler:install'
 before :'deploy:cleanup', :'boston_library:upload_gemfile'
 after :'deploy:cleanup', :'boston_library:binstubs_bundler'
 after :'boston_library:binstubs_bundler', :"boston_library:restart_#{fetch(:application)}_puma"
