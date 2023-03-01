b02878876b2dedd7597a652020375690f510b5f5
miao-bpl <rmiao@bpl.org>
2023-03-01 15:40:13 -0500

    Create a task for system service to use latest ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,6 +52,20 @@ namespace :boston_library do
     end
   end
 
+  ## Update ruby version for systemd service
+  desc "Update ruby version for systemd service"
+  task :update_service_ruby do
+    on roles(:app) do
+      execute("sudo rm /etc/systemd/system/\"#{fetch(:application)}\"_puma.service.d/override.conf | true
+              SERVICE_RUBY_VERSION=`cat /home/deployer/railsApps/\"#{fetch(:application)}\"/current/.ruby-version`
+              echo \"SERVICE_RUBY_VERSION IS: \" ${SERVICE_RUBY_VERSION}              
+              echo '[Service]' > override.conf
+              echo \"Environment=SERVICE_RUBY_VERSION=${SERVICE_RUBY_VERSION}\" >> override.conf
+              sudo mv override.conf /etc/systemd/system/\"#{fetch(:application)}\"_puma.service.d/override.conf
+              sudo /bin/systemctl daemon-reload")
+    end
+  end 
+
   desc 'Copy Gemfile and Gemfile.lock to shared directory'
   task :upload_gemfile do
     on roles(:app) do
@@ -83,5 +97,6 @@ after :'boston_library:rvm_install_ruby', :'boston_library:install_bundler'
 after :'boston_library:install_bundler', :'bundler:config'
 after :'bundler:config', :'bundler:install'
 before :'deploy:cleanup', :'boston_library:upload_gemfile'
-after :'deploy:cleanup', :"boston_library:restart_#{fetch(:application)}_puma"
+after :'deploy:cleanup', :'boston_library:update_service_ruby'
+after :'boston_library:update_service_ruby', :"boston_library:restart_#{fetch(:application)}_puma"
 after :"boston_library:restart_#{fetch(:application)}_puma", :'boston_library:restart_nginx'
