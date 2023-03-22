6391d2d424e0465aef2488161f989f9acfc2b9f0
Eben English <ebenenglish@users.noreply.github.com>
2023-03-22 16:25:50 -0400

    login view fix (#63)
    * initial work on login layout fixes [skip ci]
    
    * layout fixes

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.default_url_options = { host: 'dc3dev.bpl.org' }
+  config.action_mailer.default_url_options = { host: 'search-dc3dev.bpl.org' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
