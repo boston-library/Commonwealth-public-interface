88a572bc4bc7e2da2ec3b22dc918cb55bc213aec
miao-bpl <rmiao@bpl.org>
2023-12-13 15:29:29 -0500

    assets:precompile

--- config/deploy.rb
+++ config/deploy.rb
@@ -97,8 +97,10 @@ namespace :boston_library do
       as fetch(:user) do
         within release_path do
           puts capture("cd #{release_path}; #{fetch(:rvm_installed)} #{fetch(:rvm_ruby_version)} do ruby --version")
-          execute("cd #{release_path}; #{fetch(:rvm_installed)} #{fetch(:rvm_ruby_version)} do RAILS_ENV=#{fetch(:stage_case)} rake assets:clean")
-          execute("cd #{release_path}; #{fetch(:rvm_installed)} #{fetch(:rvm_ruby_version)} do RAILS_ENV=#{fetch(:stage_case)} rake assets:precompile")
+          # execute("cd #{release_path}; #{fetch(:rvm_installed)} #{fetch(:rvm_ruby_version)} do RAILS_ENV=#{fetch(:stage_case)} bundle exec rake assets:clean")
+          # execute("cd #{release_path}; #{fetch(:rvm_installed)} #{fetch(:rvm_ruby_version)} do RAILS_ENV=#{fetch(:stage_case)} bundle exec rake assets:precompile")
+          execute("cd #{release_path}; RAILS_ENV=#{fetch(:stage_case)} bundle exec rake assets:clean")
+          execute("cd #{release_path}; RAILS_ENV=#{fetch(:stage_case)} bundle exec rake assets:precompile")
           # execute("cd #{release_path}; #{fetch(:rvm_installed)} #{fetch(:rvm_ruby_version)} do RAILS_ENV=#{fetch(:stage_case)} bin/rails assets:clean")
           # execute("cd #{release_path}; #{fetch(:rvm_installed)} #{fetch(:rvm_ruby_version)} do RAILS_ENV=#{fetch(:stage_case)} bin/rails assets:precompile")
         end
