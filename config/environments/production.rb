a096e04c26c5f07c0320c6bd3710206b1cee2398
Eben English <eben.english@gmail.com>
2024-04-09 16:57:41 -0400

    add postmark API token and config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "commonwealth_public_interface_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :postmark
+  config.action_mailer.postmark_settings = { api_token: Rails.application.credentials.postmark_api_token }
   config.action_mailer.default_url_options = { host: 'digitalcommonwealth.org' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
