8e23068af0363062b6a6a705808a8b40f3621399
Eben English <eben.english@gmail.com>
2024-09-13 16:44:23 -0400

    survey modal popup working and styled

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def survey_cookie_name
+    "_#{I18n.t('blacklight.application_name').delete(' ')}_survey_seen"
+  end
+
   def render_survey_modal?
-    cookies[:dc_survey_seen].nil?
+    cookies[survey_cookie_name].nil?
   end
 end
