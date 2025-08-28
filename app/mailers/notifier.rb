5c19066d307c2414e11cf1e052161d2d384808a4
Eben English <eben.english@gmail.com>
2025-08-28 10:57:45 -0400

    remove all DC admin contact stuff

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,13 +2,4 @@
 
 class Notifier < ActionMailer::Base
   include CommonwealthVlrEngine::Notifier
-
-  private
-
-  # adds a few additional routing options
-  def route_email(topic)
-    return CONTACT_EMAILS['dc_admin'] if topic == t('blacklight.feedback.form.topic.options.membership.option')
-
-    super
-  end
 end
